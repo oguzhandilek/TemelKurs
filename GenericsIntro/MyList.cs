54f6289ffea41e265c793787fbad7028286b03cf
MONSTER <oguzhandilek25@gmail.com>
2023-12-24 22:40:45 +0300

    example of polymorphism

--- GenericsIntro/MyList.cs
+++ GenericsIntro/MyList.cs
@@ -23,7 +23,7 @@ namespace GenericsIntro
                 items[i] = tempArray[i];
             }
             items[items.Length-1] = item;
-        } 
-
+        }
+       
     }
 }
---
+++ OOP3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasitKredisiManager = new TasitKredisiManager();
+            IKrediManager konutKredisiManager =new KonutKredisiManager();
+
+
+            BasvuruManager  basvuruManager = new BasvuruManager();
+            //basvuruManager.BasvuruYap(tasitKredisiManager);
+
+            List<IKrediManager> krediler=new List<IKrediManager>() { ihtiyacKrediManager,tasitKredisiManager};
+
+            basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+            
+            Console.ReadLine();
+        }
+    }
+}

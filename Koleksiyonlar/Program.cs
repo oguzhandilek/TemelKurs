427cb959f6b0d5d1bef181fc5d39de90a26bc8e9
MONSTER <oguzhandilek25@gmail.com>
2023-12-24 15:06:52 +0300

    Deneme

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koleksiyonlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string> { "Oğuzhan","batuhan","Sıdıka","Sütlaç"};
+            foreach (string l in list)
+            {
+                Console.WriteLine(l);
+                
+            }
+            list.Add("Rumeysa");
+            Console.WriteLine(list[4]);
+            Console.WriteLine(list[0]);
+            Console.ReadLine();
+        }
+    }
+}
